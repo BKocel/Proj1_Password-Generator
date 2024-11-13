9fe897ba5ef9c460100ba5bab3c4effce1451708
BKocel <bartosz.kocel@outlook.com>
2024-11-13 13:01:00 +0100

    Still working on generator

--- 1_Gen haseł/main.py
+++ 1_Gen haseł/main.py
@@ -2,28 +2,37 @@ import random
 import math
 import datetime
 
+# Generators
+
+time1 = 
+
 def dictionary(wcount):
     print("Function 'dictionary' was called, but it is not implemented")
     return(1)
 
 def symbols(scount):
+    char = []
     print("Function 'symbols' was called, but it is not implemented")
     return(1)
 
+
 def random(nrcount):
+
     print("Function 'random' was called, but it is not implemented")
     return(1)
 
+# CLI
+
 print("Witaj w generatorze haseł")
+print()
 print("Oto mozliwe metody losowania")
+print()
 print("1. Numery")
 print("2. Wyrazy")
 print("3. Znaki i Numery")
 print("4.Wyrazy z numerami")
 
-mode = int(input("Wybierz tryb pracy generatora"))
-
-
+mode = int(input("Wybierz tryb pracy generatora: "))
 
 
 
