39b969b9eaa5843bd7653a2419a3959788efa214
BKocel <bartosz.kocel@outlook.com>
2024-11-13 12:14:29 +0100

    .

---
+++ 1_Gen haseł/main.py
@@ -0,0 +1,13 @@
+import random
+import math
+
+def dictionary(wcount):
+    print("Function 'dictionary' was called, but it is not implemented")
+    return(1)
+
+def random(nrcount):
+    print("Function 'random' was called, but it is not implemented")
+    return(1)
+
+print("Witaj w generatorze haseł")
+print("Oto mozliwe metody losowania")
