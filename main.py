0d0adf2219c1672fb2d5b3c169ba6d24878bcc78
Bartosz Kocel <bartosz.kocel@outlook.com>
2024-11-13 19:03:07 +0100

    Added ASCII symbol generation

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def currentkey():
     keyt = dayt * sect
     keyt2 = keyt * msec
     random.seed(keyt2)
+    # print(keyt2) - FOR DEBUG ONLY
+
 
 # CLI
 
@@ -19,31 +21,49 @@ print()
 print("Oto mozliwe metody losowania")
 print()
 print("1. Numery")
-print("2. Wyrazy")
-print("3. Znaki i Numery")
+print("2. Symbole")
+print("3. Symbole i Numery")
 print("4. Wyrazy z numerami")
 
 mode = int(input("Wybierz tryb pracy generatora: "))
 
-
+# Generators 
 
 match mode:
     case 1:
-        times = int(input("Podaj długość liczby: "))
+        times = int(input("Podaj długość hasła: "))
         numberlist = []
         i = 0
-        print("Proszę czekać, generowanie liczby, to może chwilę potrwać")
+        print("Proszę czekać, generowanie hasła...")
+        currentkey()
         while i in range(times):
-            currentkey()
+            
             rand = random.randrange(1,10)
             numberlist.append(rand)
             i+=1
-        print()
+        
         out = ''.join(str(x) for x in numberlist)
+        print("Twoje nowe hasło to: ", end = '')
         print(out)
 
     case 2:
-        print()
+        times = int(input("Podaj długość hasła: "))
+        password = []
+        i = 0
+        print("Proszę czekać, generowanie hasła...")
+        currentkey()
+        while i in range(times):
+            rand = int(random.randrange(65,122))
+            if rand == 10:
+                rand = 1
+                
+            else:
+                password.append(chr(rand))
+            i+=1
+        
+        out = ''.join(str(x) for x in password)
+        print("Twoje nowe hasło to: ", end = '')
+        print(out)
     case 3:
         print()
     case 4: 
