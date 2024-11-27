d2011c0ed364baaeaf6971309738b4f699407fe3
BKocel <bartosz.kocel@outlook.com>
2024-11-27 12:51:33 +0100

    Finished Project, ready for further testing

--- main.py
+++ main.py
@@ -22,77 +22,75 @@ print()
 print("1. Numery")
 print("2. Symbole")
 print("3. Symbole i Numery")
-print("4. Wyrazy z numerami")
+
 
 mode = int(input("Wybierz tryb pracy generatora: "))
 
 # Generators 
-
-match mode:
-    case 1: # numbers generation
-        times = int(input("Podaj długość hasła: "))
-        numberlist = []
-        i = 0
-        print("Proszę czekać, generowanie hasła...")
-        currentkey()
-        while i in range(times):
+times = int(input("Podaj długość hasła: "))
+if times <= 0:
+    print("Niepoprawny argument, proszę spróbować ponownie [Kod błędu: 01]")
+else:
+    match mode:
+        case 1: # numbers generation
             
-            rand = random.randrange(1,10)
-            numberlist.append(rand)
-            i+=1
-        
-        out = ''.join(str(x) for x in numberlist)
-        print("Twoje nowe hasło to: ", end = '')
-        print(out)
-    case 2: # ASCII symbol generation (alphabet only)
-        times = int(input("Podaj długość hasła: "))
-        password = []
-        i = 0
-        print("Proszę czekać, generowanie hasła...")
-        currentkey()
-        while i in range(times):
-            rand = int(random.randrange(65,122))
-            if rand == 10:
-                rand = 1
+            numberlist = []
+            i = 0
+            print("Proszę czekać, generowanie hasła...")
+            currentkey()
+            while i in range(times):
                 
-            else:
-                password.append(chr(rand))
-            i+=1
-        
-        out = ''.join(str(x) for x in password)
-        print("Twoje nowe hasło to: ", end = '')
-        print(out)
-
-    case 3: #TODO Combined number and ASCII Symbol generation
-        times = int(input("Podaj długość hasła: "))
-        print(times) # DEBUG
-        password = []
-        i=0
-        print("Proszę czeka, generowanie hasła...")
-        currentkey()
-        while i in range(times):
-            rand1 = chr(int(random.randrange(65,122)))  # leters generation
-            rand2 = int(random.randrange(0,10)) # numbers generation, dis not wokrs :/
-            order = int(random.uniform(0,2)) # randomly gives 0 or 1
-            if order == 1:
-                password.append(rand1)
-                password.append(rand2)
-            else:
-                password.append(rand2)
-                password.append(rand1)
-            i+=1
-        if len(password) == times :
-            out = ''.join(str(x) for x in password)
+                rand = random.randrange(1,10)
+                numberlist.append(rand)
+                i+=1
+            
+            out = ''.join(str(x) for x in numberlist)
             print("Twoje nowe hasło to: ", end = '')
             print(out)
-        else:
-            print("NIEPRAWIDŁOWA DŁUGOŚĆ")
+        case 2: # ASCII symbol generation (alphabet only)
+            
+            password = []
+            i = 0
+            print("Proszę czekać, generowanie hasła...")
+            currentkey()
+            while i in range(times):
+                rand = int(random.randrange(65,122))
+                if rand == 10:
+                    rand = 1
+                    
+                else:
+                    password.append(chr(rand))
+                i+=1
+            
             out = ''.join(str(x) for x in password)
             print("Twoje nowe hasło to: ", end = '')
             print(out)
 
-        
-    case 4: #TODO 
-        print() 
-    case _ : # Unexpected handler
-        print("Niepoprawna opcja, proszę spróbować ponownie.")
+        case 3: #TODO Combined number and ASCII Symbol generation
+            
+            # print(times) # for DEBUG
+            password = []
+            i=0
+            print("Proszę czeka, generowanie hasła...")
+            currentkey()
+            while i in range(times):
+                rand1 = chr(int(random.randrange(65,122)))  # leters generation
+                rand2 = int(random.randrange(0,10)) # numbers generation, dis not wokrs :/
+                order = random.random()# randomly gives 0 or 1
+                if order < 0.5:
+                    password.append(rand1)
+                else:
+                    password.append(rand2)
+                i+=1
+            if len(password) == times :
+                out = ''.join(str(x) for x in password)
+                print("Twoje nowe hasło to: ", end = '')
+                print(out)
+            else:
+                print("NIEPRAWIDŁOWA DŁUGOŚĆ")
+                out = ''.join(str(x) for x in password)
+                print("Twoje nowe hasło to: ", end = '')
+                print(out)
+
+        case _ : # Unexpected handler
+            print("Niepoprawna opcja, proszę spróbować ponownie. [Kod błędu: 02]")
